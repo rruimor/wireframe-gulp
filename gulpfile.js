a4522477434407345e17fd8a2394a989a16d70e4
Rafael Ruiz <raf.ruiz.moreno@gmail.com>
2017-10-12 16:20:07 +0200

    WIP Header and structured css

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('sass', function() {
+  return sass('app/scss/styles.scss')
+    .pipe(gulp.dest('app/css'))
+    .pipe(reload({ stream: true }));
+});
+
+// watch Sass files for changes, run the Sass preprocessor with the 'sass' task
+// and reload
+gulp.task('serve', ['sass'], function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch('app/scss/*.scss', ['sass']);
+  gulp.watch('*.html', { cwd: 'app' }, reload);
+});
